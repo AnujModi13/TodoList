b733f39f6af7cf889bf09d35204d719ed428f20b
Anuj <modianuj1376@gmail.com>
2023-11-26 20:16:36 +0530

    vercel api

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,7 +1,7 @@
 import { ITask } from './model/task';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
@@ -11,21 +11,21 @@ export class ApiService {
 
 
   getTodo():any{
-  let url='https://todoserver-production-6d87.up.railway.app/api/todo/';
+  let url=environment.apiUrl;
   return this.http.get<ITask>(url);
 }
 
   postTodo(body:any){
-    let url='https://todoserver-production-6d87.up.railway.app/api/todo/';
+    let url=environment.apiUrl;
     return this.http.post<ITask>(url,body,{responseType:'json'});
   }
 
   deleteTask(id:any){
-    let url='https://todoserver-production-6d87.up.railway.app/api/todo/'+id;
+    let url=environment.apiUrl+id;
     return this.http.delete<ITask>(url);
   }
   updateTask(id:any,body:any){
-    let url='https://todoserver-production-6d87.up.railway.app/api/todo/'+id;
+    let url=environment.apiUrl+id;
     return this.http.patch<ITask>(url,body,{responseType:'json'});
   }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  apiUrl:'https://todo-server-steel.vercel.app/api/todo/',
+};
